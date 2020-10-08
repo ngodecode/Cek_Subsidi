90a4be8034221a50bfd7b52dacc5ec6223c272f6
Febri Arianto <felixarianto13@gmail.com>
2020-10-08 22:45:17 +0700

    ubah url pelanggan

--- app/src/main/java/com/ftools/ceksubsidi/CekSubsidiFragment.kt
+++ app/src/main/java/com/ftools/ceksubsidi/CekSubsidiFragment.kt
@@ -99,12 +99,12 @@ class CekSubsidiFragment : Fragment() {
         mClient = builder.addInterceptor {
                 chain -> chain.proceed(
             chain.request().newBuilder()
-                .addHeader("X-GWT-Module-Base", "https://layanan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/")
+                .addHeader("X-GWT-Module-Base", "https://pelanggan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/")
                 .addHeader("X-GWT-Permutation", "A1630C2E2A3C73EFF36ABD53F8C9A151")
                 .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
                 .addHeader("Content-Type", "text/x-gwt-rpc; charset=UTF-8")
                 .addHeader("Accept", "*/*")
-                .addHeader("Origin", "https://layanan.pln.co.id")
+                .addHeader("Origin", "https://pelanggan.pln.co.id")
                 .build()
         )
         }.build()
@@ -330,7 +330,7 @@ class CekSubsidiFragment : Fragment() {
     }
 
     fun getAPIService() : APIService {
-        val retrofit = Retrofit.Builder().baseUrl("https://layanan.pln.co.id")
+        val retrofit = Retrofit.Builder().baseUrl("https://pelanggan.pln.co.id")
             .client(mClient!!)
             .addConverterFactory(ConverterFactory())
             .build()
@@ -367,7 +367,7 @@ class CekSubsidiFragment : Fragment() {
         mAdapterKecamatan?.notifyDataSetChanged()
         mAdapterKelurahan?.notifyDataSetChanged()
 
-        getAPIService().getArea("5|0|4|https://layanan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterProvinsi|1|2|3|4|0|")
+        getAPIService().getArea("5|0|4|https://pelanggan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterProvinsi|1|2|3|4|0|")
             .enqueue(object : Callback<String> {
                 override fun onResponse(call: Call<String>?, response: Response<String>?) {
                     Log.w(mTag, response?.body() ?: "NULL")
@@ -411,7 +411,7 @@ class CekSubsidiFragment : Fragment() {
         mAdapterKecamatan?.notifyDataSetChanged()
         mAdapterKelurahan?.notifyDataSetChanged()
 
-        getAPIService().getArea("5|0|6|https://layanan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterKabupatenByKdProv|java.lang.String/2004016611|$areaId|1|2|3|4|1|5|6|")
+        getAPIService().getArea("5|0|6|https://pelanggan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterKabupatenByKdProv|java.lang.String/2004016611|$areaId|1|2|3|4|1|5|6|")
             .enqueue(object : Callback<String> {
                 override fun onResponse(call: Call<String>?, response: Response<String>?) {
                     Log.w(mTag, response?.body() ?: "NULL")
@@ -453,7 +453,7 @@ class CekSubsidiFragment : Fragment() {
         mAdapterKecamatan?.notifyDataSetChanged()
         mAdapterKelurahan?.notifyDataSetChanged()
 
-        getAPIService().getArea("5|0|6|https://layanan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterKecamatanByKdKab|java.lang.String/2004016611|$areaId|1|2|3|4|1|5|6|")
+        getAPIService().getArea("5|0|6|https://pelanggan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterKecamatanByKdKab|java.lang.String/2004016611|$areaId|1|2|3|4|1|5|6|")
             .enqueue(object : Callback<String> {
                 override fun onResponse(call: Call<String>?, response: Response<String>?) {
                     Log.w(mTag, response?.body() ?: "NULL")
@@ -493,7 +493,7 @@ class CekSubsidiFragment : Fragment() {
         mListKelurahan.clear()
         mAdapterKelurahan?.notifyDataSetChanged()
 
-        getAPIService().getArea("5|0|6|https://layanan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterDesaByKdKec|java.lang.String/2004016611|$areaId|1|2|3|4|1|5|6|")
+        getAPIService().getArea("5|0|6|https://pelanggan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|F70866662104DD987A6DE8B1B78CD0FF|id.co.iconpln.web.client.service.MasterService|getMasterDesaByKdKec|java.lang.String/2004016611|$areaId|1|2|3|4|1|5|6|")
             .enqueue(object : Callback<String> {
                 override fun onResponse(call: Call<String>?, response: Response<String>?) {
                     Log.w(mTag, response?.body() ?: "NULL")
@@ -572,7 +572,7 @@ class CekSubsidiFragment : Fragment() {
 
         Log.w(mTag, "areaId=$areaId / ktp=$ktp")
         getAPIService().getStatus(mCookie!!,
-            "5|0|12|https://layanan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|AB6BB8F2A9B546B9DE2B259C9242D117|id.co.iconpln.web.client.service.TransService|getValidasiTarifDaya|D|java.lang.String/2004016611|R||52201|PASANG BARU|$ktp|$areaId|1|2|3|4|9|5|6|5|6|6|6|6|5|6|8|7|450|8|9|10|11|0|12|"
+            "5|0|12|https://pelanggan.pln.co.id/id.co.iconpln.web.PBMohonEntryPoint/|AB6BB8F2A9B546B9DE2B259C9242D117|id.co.iconpln.web.client.service.TransService|getValidasiTarifDaya|D|java.lang.String/2004016611|R||52201|PASANG BARU|$ktp|$areaId|1|2|3|4|9|5|6|5|6|6|6|6|5|6|8|7|450|8|9|10|11|0|12|"
         ).enqueue(object : Callback<String> {
 
             override fun onResponse(call: Call<String>?, response: Response<String>?) {
